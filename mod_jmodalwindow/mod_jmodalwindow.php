4919fe28ab22f70d5b3835a5de1361742dc84ff7
Miguel Puig <miguelpuig@gmail.com>
2013-06-14 16:29:29 +0100

    added cache

--- mod_jmodalwindow/mod_jmodalwindow.php
+++ mod_jmodalwindow/mod_jmodalwindow.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @package		Joomla.Site
- * @subpackage	mod_weblinks
- * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- */
 
 // no direct access
 defined('_JEXEC') or die;
@@ -12,12 +6,9 @@ defined('_JEXEC') or die;
 // Include the weblinks functions only once
 require_once dirname(__FILE__) . '/helper.php';
 
-$list = modJmodalwindowHelper::getList($params);
+//$list = modJmodalwindowHelper::getList($params);
 
-if (!count($list)) {
-	return;
-}
-
+JHTML::_('behavior.modal');
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 require JModuleHelper::getLayoutPath('mod_jmodalwindow', $params->get('layout', 'default'));
--- mod_jmodalwindow/tmpl/default.php
+++ mod_jmodalwindow/tmpl/default.php
@@ -1,6 +1,92 @@
 <?php
-
-
+// No direct access to this file
 defined('_JEXEC') or die;
+
+$conflict_load = $params->get('conflict_load',1);
+$showScroll =  $params->get('scrollbars',1);
+if($showScroll):
+    $scrolling = 'yes';
+else:
+    $scrolling = 'no';
+endif;
+
+    //Cookie control
+    $days = $params->get('cookietime',1);
+
+
 ?>
-modal...
+<script type="text/javascript">
+
+
+    var ToolsModalRTVNAdv<?php echo $module->id;?> = {
+        createCookie: function(name, value,days) {
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime()+(days*24*60*60*1000));
+                var expires = "; expires="+date.toGMTString();
+            }else var expires = "";
+            document.cookie = name+"="+value+expires+"; path=/";
+        },
+
+        readCookie: function(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        },
+
+        eraseCookie: function(name) {
+            ToolsModalRTVNAdv<?php echo $module->id;?>.createCookie(name,"",-1);
+        }
+    };
+
+
+
+    (function($){
+
+
+        $.fn.rtvnModalcwAllowCookiesAdv<?php $module->id;?> = function() {
+
+            var CookieExistsrtvn<?php echo $module->id;?>=ToolsModalRTVNAdv<?php echo $module->id;?>.readCookie("<?php echo 'rtvnmodalAdv'.$module->id;?>");
+
+
+            //Check if cookie exists
+            if (CookieExistsrtvn<?php echo $module->id;?>) {
+
+            } else {
+                ToolsModalRTVNAdv<?php echo $module->id;?>.createCookie("<?php echo 'rtvnmodalAdv'.$module->id;?>",true);
+                SqueezeBox.open("<?php echo $params->get('url','http://google.com');?>", {
+                    handler: 'iframe',
+                    size: { x: <?php echo $params->get('width',800);?>, y: <?php echo $params->get('height',600);?> },
+                    iframeOptions: {scrolling: '<?php echo $scrolling;?>'}
+                });
+
+            }
+
+        };
+
+    })(jQuery);
+
+</script>
+
+
+<script type="text/javascript">
+
+    <?php if($conflict_load == 1) { ?>
+    var $rtvnmodalAdv = jQuery.noConflict();
+    $rtvnmodalAdv(function(){
+        $rtvnmodalAdv('body').rtvnModalcwAllowCookiesAdv<?php $module->id;?>();
+    });
+    <?php } else { ?>
+
+    $(function(){
+        $('body').rtvnModalcwAllowCookiesAdv<?php $module->id;?>();
+    });
+    <?php } ?>
+
+
+</script>
