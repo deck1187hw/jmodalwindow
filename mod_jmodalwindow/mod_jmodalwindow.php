28cf3d4b7d5d0b9fd9a19145df10e92f4c2ffa4e
Miguel Puig <miguelpuig@gmail.com>
2013-06-14 11:11:41 +0100

    added basic files

--- mod_jmodalwindow/mod_jmodalwindow.php
+++ mod_jmodalwindow/mod_jmodalwindow.php
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 
 // Include the weblinks functions only once
-require_once dirname(__FILE__).'/helper.php';
+require_once dirname(__FILE__) . '/helper.php';
 
 $list = modJmodalwindowHelper::getList($params);
 
